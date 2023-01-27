bf0d4b430306e8ad14c488b6cfdcb481ac0d264c
CAMM961001 <miguel.castama@gmail.com>
2023-01-27 15:28:33 -0600

    Agregar resultados a archivo JSON

--- servicios/etl/etl.py
+++ servicios/etl/etl.py
@@ -1,4 +1,4 @@
-import os, sys, json, yaml
+import os, json, yaml, sys
 
 CURRENT = os.path.dirname(os.path.abspath(__file__))
 PARENT = os.path.dirname(CURRENT)
@@ -6,25 +6,54 @@ ROOT = os.path.dirname(PARENT)
 sys.path.append(PARENT)
 
 import consultas
-import utilidades as utils
 
-#Archivo de configuración
+# Archivo de configuración
 with open(os.path.join(ROOT, 'config.yaml'), 'r') as f:
     config = yaml.safe_load(f)
 f.close()
 
-#Archivo de criterios de búsqueda
+# Archivo de criterios de búsqueda
 with open(file=os.path.join(ROOT, config['etl']['criterios']), mode='r') as f:
     criterios_busqueda = [row.strip() for row in f]        
 f.close()
 
-#Archivo de corpus de consulta
-with open(file=os.path.join(ROOT, config['etl']['corpus']), mode='r') as f:
+# Archivo de corpus de consulta
+CORPUS_PATH = os.path.join(ROOT, config['etl']['corpus'])
+with open(file=CORPUS_PATH, mode='r') as f:
     corpus = json.load(f)
 f.close()
 
-#Criterios existentes en corpus
+# Criterios existentes en corpus
 existencias = [corpus[idx]['criterio'] for idx in range(len(corpus))]
 
+
+def etl():
+    """
+    
+    """
+    for criterio in criterios_busqueda:
+        
+        # ---- Criterio de búsqueda nuevo ----
+        if criterio not in existencias:
+
+            # Leer archivo JSON
+            listObj = list()
+            with open(CORPUS_PATH) as f:
+                listObj = json.load(f)
+
+            estructura = consultas.consulta_completa(criterio=criterio, anio=2, meses=3)
+                
+            listObj.append(estructura)
+            
+            with open(CORPUS_PATH, 'w') as json_file:
+                json.dump(listObj, json_file, separators=(',',': '))
+            print(f"Se agregó existosamente criterio: {criterio}")
+        
+        # ---- Criterio de búsqueda existente ----
+        else:
+            print(criterio, '\texistente')
+
+
+
 if __name__ == '__main__':
-    print(existencias)
+    etl()
