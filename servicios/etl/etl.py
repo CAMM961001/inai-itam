eaf20d22d859d38754d197a44ef6cdd9d3194541
CAMM961001 <miguel.castama@gmail.com>
2023-01-27 14:02:43 -0600

    Lista de criterios existentes

--- servicios/etl/etl.py
+++ servicios/etl/etl.py
@@ -1,19 +1,30 @@
-import os, sys
+import os, sys, json, yaml
 
 CURRENT = os.path.dirname(os.path.abspath(__file__))
 PARENT = os.path.dirname(CURRENT)
 ROOT = os.path.dirname(PARENT)
 sys.path.append(PARENT)
 
-import json
-import yaml
+import consultas
 import utilidades as utils
 
-#Archivo de configuracion
-with open(os.path.join(ROOT, "config.yaml"), "r") as file:
-    config = yaml.safe_load(file)
+#Archivo de configuración
+with open(os.path.join(ROOT, 'config.yaml'), 'r') as f:
+    config = yaml.safe_load(f)
+f.close()
 
+#Archivo de criterios de búsqueda
+with open(file=os.path.join(ROOT, config['etl']['criterios']), mode='r') as f:
+    criterios_busqueda = [row.strip() for row in f]        
+f.close()
 
-if __name__ == "__main__":
-    
-    print(CURRENT, PARENT, ROOT)
+#Archivo de corpus de consulta
+with open(file=os.path.join(ROOT, config['etl']['corpus']), mode='r') as f:
+    corpus = json.load(f)
+f.close()
+
+#Criterios existentes en corpus
+existencias = [corpus[idx]['criterio'] for idx in range(len(corpus))]
+
+if __name__ == '__main__':
+    print(existencias)
