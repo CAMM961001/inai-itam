153430ec051d33bd058ac6392e8ddf28275e1c97
CAMM961001 <miguel.castama@gmail.com>
2023-02-17 13:46:50 -0600

    Logging agregado a servicio

--- servicios/etl.py
+++ servicios/etl.py
@@ -14,6 +14,14 @@ with open(os.path.join(ROOT, 'config.yaml'), 'r') as f:
     config = yaml.safe_load(f)
 f.close()
 
+# Configuración de logs
+logging.basicConfig(
+    filename=config['logs']['etl'],
+    level=logging.INFO,
+    filemode='a',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    format='%(levelname)s|%(asctime)s|%(name)s|\n%(message)s')
+
 # Archivo de criterios de búsqueda
 with open(file=os.path.join(ROOT, config['datos']['criterios']), mode='r') as f:
     criterios_busqueda = [row.strip() for row in f]        
@@ -33,31 +41,36 @@ def etl():
     """
     
     """
+    prompt = f"Trabajo: {__file__}\n"
     for criterio in criterios_busqueda:
-        
-        # ---- Criterio de búsqueda nuevo ----
-        if criterio not in existencias:
-
-            # Leer archivo JSON
-            listObj = list()
-            with open(CORPUS_PATH) as f:
-                listObj = json.load(f)
-                consulta = consultas.consulta_completa(criterio=criterio, anio=2, meses=3)
-                listObj.append(consulta)
-            f.close()
+        try:
+            # ---- Criterio de búsqueda nuevo ----
+            if criterio not in existencias:
+                
+                # Leer archivo JSON
+                listObj = list()
+                with open(CORPUS_PATH) as f:
+                    listObj = json.load(f)
+                    consulta = consultas.consulta_completa(criterio=criterio, anio=2, meses=3)
+                    listObj.append(consulta)
+                f.close()
+                
+                # Escribir resultados de consulta en corpus
+                with open(CORPUS_PATH, 'w') as json_file:
+                    json.dump(listObj, json_file, separators=(',',': '))
+                json_file.close()
+                
+                # Registrar estado de consulta en log
+                prompt += f'\t- "{criterio}" agregado\n'
             
-            # Escribir resultados de consulta en corpus
-            with open(CORPUS_PATH, 'w') as json_file:
-                json.dump(listObj, json_file, separators=(',',': '))
-            json_file.close()
-            
-            # Registrar estado de consulta en log
-            print(f"Se agregó existosamente criterio: {criterio}")
+            # ---- Criterio de búsqueda existente ----
+            else:
+                prompt += f'\t- "{criterio}" existente\n'
         
-        # ---- Criterio de búsqueda existente ----
-        else:
-            print(criterio, '\texistente')
+        except Exception:
+            break
 
+    logging.info(prompt)
 
 
 if __name__ == '__main__':
