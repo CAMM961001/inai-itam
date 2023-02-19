7e50dc8da4bd0b934b636014ed028075d5ef5bbb
CAMM961001 <miguel.castama@gmail.com>
2023-02-19 10:28:28 -0600

    Estructura nueva de consultas - interes tiempo

--- servicios/etl.py
+++ servicios/etl.py
@@ -4,6 +4,8 @@ import yaml
 import logging
 
 import modulos.consultas as consultas
+import utilidades as utils
+
 
 # Paths absolutos
 CURRENT = os.path.dirname(os.path.abspath(__file__))
@@ -14,6 +16,11 @@ with open(os.path.join(ROOT, 'config.yaml'), 'r') as f:
     config = yaml.safe_load(f)
 f.close()
 
+# PAths de script
+TIEMPO_PATH = os.path.join(ROOT, config['datos']['tiempo'])
+REGION_PATH = os.path.join(ROOT, config['datos']['region'])
+RELACION_PATH = os.path.join(ROOT, config['datos']['relacionados'])
+
 # Configuración de logs
 logging.basicConfig(
     filename=config['logs']['etl'],
@@ -22,56 +29,97 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S',
     format='%(levelname)s|%(asctime)s|%(name)s|\n%(message)s')
 
-# Archivo de criterios de búsqueda
+# Cargar criterios de búsqueda en memoria
 with open(file=os.path.join(ROOT, config['datos']['criterios']), mode='r') as f:
     criterios_busqueda = [row.strip() for row in f]        
 f.close()
 
-# Archivo de corpus de consulta
-CORPUS_PATH = os.path.join(ROOT, config['datos']['corpus'])
-with open(file=CORPUS_PATH, mode='r') as f:
+# Periodos de consulta
+periodos = utils.generar_periodos(
+    anios=config['etl']['anios'],
+    meses=config['etl']['meses'])
+
+# Enlistar criterios existentes
+with open(file=TIEMPO_PATH, mode='r') as f:
     corpus = json.load(f)
 f.close()
-
-# Criterios existentes en corpus
 existencias = [corpus[idx]['criterio'] for idx in range(len(corpus))]
 
 
-def etl():
-    """
-    
-    """
-    prompt = f"Trabajo: {__file__}\n"
-    for criterio in criterios_busqueda:
-        try:
-            # ---- Criterio de búsqueda nuevo ----
-            if criterio not in existencias:
-                
-                # Leer archivo JSON
-                listObj = list()
-                with open(CORPUS_PATH) as f:
-                    listObj = json.load(f)
-                    consulta = consultas.consulta_completa(criterio=criterio, anio=2, meses=3)
-                    listObj.append(consulta)
-                f.close()
-                
-                # Escribir resultados de consulta en corpus
-                with open(CORPUS_PATH, 'w') as json_file:
-                    json.dump(listObj, json_file, separators=(',',': '))
-                json_file.close()
-                
-                # Registrar estado de consulta en log
-                prompt += f'\t- "{criterio}" agregado\n'
-            
-            # ---- Criterio de búsqueda existente ----
-            else:
-                prompt += f'\t- "{criterio}" existente\n'
-        
-        except Exception:
-            break
+# Extracción de información y almacenamiento
+for criterio in criterios_busqueda:
 
-    logging.info(prompt)
+    # ---- Interés en el tiempo ----
+    estructura = {
+        "criterio":criterio,
+        "contenidos":[]
+        }
+    
+    consulta = consultas.Consultar(
+        criterio=criterio,
+        inicio=periodos[0][0],
+        fin=periodos[0][1],
+        ventana=config['etl']['anios'])
+
+    # Criterios nuevos en corpus
+    if criterio not in existencias:
+        print(criterio)
+
+        # Leer archivo JSON
+        listObj = list()
+        with open(TIEMPO_PATH, 'r') as f:
+            listObj = json.load(f)
+            estructura['contenidos'] = consulta.interes_tiempo()
+            listObj.append(estructura)
+        f.close()
+        
+        # Escribir resultados de consulta en corpus
+        with open(TIEMPO_PATH, 'w') as json_file:
+            json.dump(listObj, json_file, separators=(',',': '))
+        json_file.close()
+
+
+
+# # Criterios existentes en corpus
+
+
+# def etl():
+#     """
+    
+#     """
+#     prompt = f"Trabajo: {__file__}\n"
+#     for criterio in criterios_busqueda:
+        
+#         # ---- Criterio de búsqueda nuevo ----
+#         if criterio not in existencias:        
+#             try:
+#                 consulta = consultas.consulta_completa(criterio=criterio, anio=2, meses=3)
+#                 prompt += f'\t- "{criterio}" agregado\n'
+            
+#             except Exception:
+#                 consulta = None
+#                 prompt += f'\t- "{criterio}" error\n'
+#                 break
+
+#             if consulta != None:
+                # # Leer archivo JSON
+                # listObj = list()
+                # with open(CORPUS_PATH, 'r') as f:
+                #     listObj = json.load(f)
+                #     listObj.append(consulta)
+                # f.close()
+                
+                # # Escribir resultados de consulta en corpus
+                # with open(CORPUS_PATH, 'w') as json_file:
+                #     json.dump(listObj, json_file, separators=(',',': '))
+                # json_file.close()
+
+#         # ---- Criterio de búsqueda existente ----
+#         else:
+#             prompt += f'\t- "{criterio}" existente\n'
+
+#     logging.info(prompt)
 
 
 if __name__ == '__main__':
-    etl()
+    print('Done')
