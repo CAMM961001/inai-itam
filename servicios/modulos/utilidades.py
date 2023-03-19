5c965e1340641acd3934e879ccd41f50b4d9e22a
CAMM961001 <miguel.castama@gmail.com>
2023-03-18 22:54:01 -0600

    Función para crear tabla de temas relacionados

--- servicios/modulos/utilidades.py
+++ servicios/modulos/utilidades.py
@@ -1,6 +1,8 @@
 import re
 import datetime as dt
+import pandas as pd
 
+from itertools import product
 from unidecode import unidecode
 from dateutil.relativedelta import relativedelta
 
@@ -63,3 +65,39 @@ def formatear_palabras(doc_, idioma='spanish'):
     
     return doc_
 
+
+def tabla_temas_relacionados(obj_json, id_lote):
+    
+    # Dataframe de resultados
+    criterios_lote = pd.DataFrame(columns=['descripcion','tipo','inicio','fin','lote','criterio','id_criterio'])
+
+    # Iteradores
+    criterios = range(len(obj_json))
+    tipos = range(len(obj_json[0]['contenidos'][id_lote]['consulta']))
+
+    for id_criterio, id_tipo in product(criterios, tipos):
+        # Parámetros de consulta en JSON
+        CRITERIO = obj_json[id_criterio]['criterio']
+        LOTE = obj_json[id_criterio]['contenidos'][id_lote]
+        INICIO = LOTE['fecha_inicio']
+        FIN = LOTE['fecha_fin']
+        TIPO = list( LOTE['consulta'].keys() )[id_tipo]
+
+        # Transformación de datos
+        df_ = (
+            pd.DataFrame(LOTE['consulta'][TIPO])
+            .filter(items=['descripcion'])
+            .assign(
+                tipo = TIPO
+                ,inicio = INICIO
+                ,fin = FIN
+                ,lote = id_lote
+                ,criterio = CRITERIO
+                ,id_criterio = id_criterio
+            )
+        )
+
+        # Agregar resultados a conjunto de análisis
+        criterios_lote = pd.concat([criterios_lote, df_], axis=0, ignore_index=True)
+
+    return criterios_lote
